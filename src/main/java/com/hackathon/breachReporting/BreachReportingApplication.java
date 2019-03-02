3c033c18c09a2c9d7082099939c1860aea950e84
prashanth <prashanth20121992@gmail.com>
2019-03-01 22:19:52 -0800

    Base project

---
+++ src/main/java/com/hackathon/breachReporting/BreachReportingApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.breachReporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreachReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreachReportingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/breachReporting/BreachReportingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hackathon.breachReporting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BreachReportingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
